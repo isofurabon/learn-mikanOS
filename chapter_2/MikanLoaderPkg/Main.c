cccc2e6edf87b143fa048717f456e8b725b553b5
isofurabon <isofurabon@users.noreply.github.com>
2021-04-07 15:04:40 +0000

    feat: add MikanLoaderPkg and related files

---
+++ chapter_2/MikanLoaderPkg/Main.c
@@ -0,0 +1,9 @@
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+
+EFI_STATUS EFI_API UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
+{
+    Print(L"Hello, Mikan World!\n");
+    while (1);
+    return EFI_SUCCESS;
+}
