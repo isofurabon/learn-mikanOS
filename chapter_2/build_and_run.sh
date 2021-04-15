271293d6a314eeff5c45384eafa68e138a1ed1a3
isofurabon <isofurabon@users.noreply.github.com>
2021-04-15 15:26:02 +0000

    feat: change script for run

--- chapter_2/build_and_run.sh
+++ chapter_2/build_and_run.sh
@@ -2,12 +2,13 @@
 
 set -e
 
+root_dir="/workspaces/learn-mikanOS"
 work_dir=$(pwd)
 edk2_dir=${HOME}/edk2
 target_config_file=../config/target.txt
 
 # create symbolic link in edk2 directory
-ln -is /workspaces/learn-mikanOS/chapter_2/MikanLoaderPkg ${edk2_dir}
+ln -is ${root_dir}/chapter_2/MikanLoaderPkg ${edk2_dir}
 
 # copy target.txt to edk2 dir
 cp -i ${target_config_file} ${edk2_dir}/Conf/target.txt
@@ -21,4 +22,7 @@ build
 
 # copy efi to here
 cp Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${work_dir}/bootx64.efi
+
+# run
+${root_dir}/tools/run_qemu.sh ${root_dir}/vm/fs
 
