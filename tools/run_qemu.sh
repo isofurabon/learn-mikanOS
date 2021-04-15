2a7bd5e6f754a0cd347ac10a64fe2f77ecd6f745
isofurabon <isofurabon@users.noreply.github.com>
2021-04-15 15:18:04 +0000

    refactor: change option

--- tools/run_qemu.sh
+++ tools/run_qemu.sh
@@ -9,7 +9,6 @@ fi
 FS_PATH=$1
 
 qemu-system-x86_64 \
-    -drive if=pflash,format=raw,readonly,file=$HOME/osbook/devenv/OVMF_CODE.fd \
-    -drive if=pflash,format=raw,file=$HOME/osbook/devenv/OVMF_VARS.fd \
-    -drive dir=${FS_PATH},driver=vvfat,rw=on \
-#    -hda disk.img -
+    -drive if=pflash,format=raw,readonly,file=${HOME}/osbook/devenv/OVMF_CODE.fd \
+    -drive if=pflash,format=raw,file=${HOME}/osbook/devenv/OVMF_VARS.fd \
+    -drive format=raw,file=fat:rw:${FS_PATH} \
