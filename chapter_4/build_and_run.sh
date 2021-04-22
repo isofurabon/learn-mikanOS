a0c82415acb1b6f37ea6ef70cc06904b4f931999
isofurabon <>
2021-04-22 15:38:55 +0000

    fix: mv to cp

--- chapter_4/build_and_run.sh
+++ chapter_4/build_and_run.sh
@@ -7,12 +7,11 @@ PROJECT_ROOT="/workspaces/learn-mikanOS"
 VM_ROOT="/workspaces/learn-mikanOS/mnt"
 EDK2_DIR=${HOME}/edk2
 OSBOOK_DIR=${HOME}/osbook
-TARGET_CONFIG_FILE=../config/target.txt
+TARGET_CONFIG_FILE=${PROJECT_ROOT}/config/target.txt
 
 # build kernel
 source ${OSBOOK_DIR}/devenv/buildenv.sh
-clang++ ${CPPFLAGS} -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c kernel/main.cpp
-ld.lld ${LDFLAGS} --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
+make
 
 # create symbolic link in edk2 directory
 ln -is ${PROJECT_ROOT}/${CHAPTER}/MikanLoaderPkg ${EDK2_DIR}
@@ -28,10 +27,10 @@ source edksetup.sh
 build
 
 # move efi to vm_root
-mv Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${VM_ROOT}/efi/boot/bootx64.efi
+cp -i ${EDK2_DIR}/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${VM_ROOT}/efi/boot/bootx64.efi
 
 # move kernel to vm_root
-mv ${PROJECT_ROOT}/${CHAPTER}/kernel.elf ${VM_ROOT}/kernel.elf
+cp -i ${PROJECT_ROOT}/${CHAPTER}/kernel/kernel.elf ${VM_ROOT}/kernel.elf
 
 # run
 ${PROJECT_ROOT}/tools/run_qemu.sh ${VM_ROOT}
