3c785bcd21b854cb8b08c5dde8f7a1e2cb0f97ad
isofurabon <isofurabon@users.noreply.github.com>
2021-04-25 09:00:32 +0000

    feat: update makefile

--- chapter_4/build_and_run.sh
+++ chapter_4/build_and_run.sh
@@ -2,17 +2,15 @@
 
 set -e
 
-CHAPTER="chapter_4"
 PROJECT_ROOT="/learn-mikanos"
+CHAPTER="chapter_4"
 VM_ROOT="${PROJECT_ROOT}/mnt"
 EDK2_DIR="${HOME}/edk2"
 TARGET_CONFIG_FILE="${PROJECT_ROOT}/config/target.txt"
 OSBOOK_DIR=${HOME}/osbook
 
 # build kernel
-(source ${OSBOOK_DIR}/devenv/buildenv.sh && \
-clang++ ${CPPFLAGS} -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c kernel/main.cpp && \
-ld.lld ${LDFLAGS} --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o)
+(source ${OSBOOK_DIR}/devenv/buildenv.sh && make -C ${PROJECT_ROOT}/${CHAPTER}/kernel all)
 
 # create symbolic link in edk2 directory
 ln -fs ${PROJECT_ROOT}/${CHAPTER}/MikanLoaderPkg ${EDK2_DIR}
@@ -27,7 +25,7 @@ cp -f ${TARGET_CONFIG_FILE} ${EDK2_DIR}/Conf/target.txt
 cp -f ${EDK2_DIR}/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${VM_ROOT}/efi/boot/bootx64.efi
 
 # move kernel to vm_root
-cp -f ${PROJECT_ROOT}/${CHAPTER}/kernel.elf ${VM_ROOT}/kernel.elf
+cp -f ${PROJECT_ROOT}/${CHAPTER}/kernel/kernel.elf ${VM_ROOT}/kernel.elf
 
 # run
 ${PROJECT_ROOT}/tools/run_qemu.sh ${VM_ROOT}
