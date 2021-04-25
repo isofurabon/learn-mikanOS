3ad5e014b4e6f0e38c9df200e49cf418aa266fd2
isofurabon <isofurabon@users.noreply.github.com>
2021-04-25 07:50:52 +0000

    feat: use subshell

--- chapter_4/build_and_run.sh
+++ chapter_4/build_and_run.sh
@@ -3,34 +3,31 @@
 set -e
 
 CHAPTER="chapter_4"
-PROJECT_ROOT="/workspaces/learn-mikanOS"
-VM_ROOT="/workspaces/learn-mikanOS/mnt"
-EDK2_DIR=${HOME}/edk2
+PROJECT_ROOT="/learn-mikanos"
+VM_ROOT="${PROJECT_ROOT}/mnt"
+EDK2_DIR="${HOME}/edk2"
+TARGET_CONFIG_FILE="${PROJECT_ROOT}/config/target.txt"
 OSBOOK_DIR=${HOME}/osbook
-TARGET_CONFIG_FILE=${PROJECT_ROOT}/config/target.txt
 
 # build kernel
-source ${OSBOOK_DIR}/devenv/buildenv.sh
-make
+(source ${OSBOOK_DIR}/devenv/buildenv.sh && \
+clang++ ${CPPFLAGS} -O2 -Wall -g --target=x86_64-elf -ffreestanding -mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c kernel/main.cpp && \
+ld.lld ${LDFLAGS} --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o)
 
 # create symbolic link in edk2 directory
-ln -is ${PROJECT_ROOT}/${CHAPTER}/MikanLoaderPkg ${EDK2_DIR}
+ln -fs ${PROJECT_ROOT}/${CHAPTER}/MikanLoaderPkg ${EDK2_DIR}
 
 # copy target.txt to edk2 dir
-cp -i ${TARGET_CONFIG_FILE} ${EDK2_DIR}/Conf/target.txt
+cp -f ${TARGET_CONFIG_FILE} ${EDK2_DIR}/Conf/target.txt
 
-# source edk
-cd ${EDK2_DIR} 
-source edksetup.sh
-
-# build
-build
+# source edk & build
+(cd ${EDK2_DIR} && source edksetup.sh && build)
 
 # move efi to vm_root
-cp -i ${EDK2_DIR}/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${VM_ROOT}/efi/boot/bootx64.efi
+cp -f ${EDK2_DIR}/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${VM_ROOT}/efi/boot/bootx64.efi
 
 # move kernel to vm_root
-cp -i ${PROJECT_ROOT}/${CHAPTER}/kernel/kernel.elf ${VM_ROOT}/kernel.elf
+cp -f ${PROJECT_ROOT}/${CHAPTER}/kernel.elf ${VM_ROOT}/kernel.elf
 
 # run
 ${PROJECT_ROOT}/tools/run_qemu.sh ${VM_ROOT}
