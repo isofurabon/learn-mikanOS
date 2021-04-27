876b02d042d81d993e66325786e56dacda8e9f60
isofurabon <isofurabon@users.noreply.github.com>
2021-04-27 15:52:11 +0000

    feat: use class for writing rectangles

--- chapter_4/kernel/main.cpp
+++ chapter_4/kernel/main.cpp
@@ -1,48 +1,86 @@
 #include <cstdint>
+#include <cstddef>
 #include "frame_buffer_config.hpp"
 
 struct PixelColor {
     uint8_t r, g, b;
 };
 
-/** WritePixel draw a pixel point with a specified color
- * @retval  0       Success
- * @retval  non-0   Fail  
- */
+class PixelWriter {
+public:
+    PixelWriter(const FrameBufferConfig& config) : m_config(config){}
+    virtual ~PixelWriter() = default;
+    virtual void Write(int x, int y, const PixelColor& c) = 0;
 
-int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& c)
-{
-    const int pixel_posiiton = config.pixels_per_scan_line * y + x;
-    uint8_t* p = &config.frame_buffer[4 * pixel_posiiton];
+protected:
+    uint8_t* PixelAt(int x, int y){
+        return m_config.frame_buffer + 4 * (m_config.pixels_per_scan_line * y + x);
+    }
 
-    if (config.pixel_format == kPixelRGBResv8BitPerColor){
+private:
+    const FrameBufferConfig& m_config;
+};
+
+class RGBResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+    // https://cpprefjp.github.io/lang/cpp11/inheriting_constructors.html
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override {
+        auto* p = PixelAt(x, y);
         p[0] = c.r;
         p[1] = c.g;
         p[2] = c.b;
-    } else if (config.pixel_format == kPixelBGRResv8BitPerColor){
+    }
+};
+
+class BGRResv8BitPerColorPixelWriter : public PixelWriter {
+public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override {
+        auto* p = PixelAt(x, y);
         p[0] = c.b;
         p[1] = c.g;
         p[2] = c.r;
-    } else {
-        return -1;
     }
+};
 
-    return 0;
+void* operator new(size_t size, void* buf) {
+    return buf;
 }
+
+void operator delete(void* obj) noexcept {
+}
+
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) 
 {
+    // create pixel writer
+    switch(frame_buffer_config.pixel_format){
+        case kPixelRGBResv8BitPerColor: 
+            pixel_writer = new(pixel_writer_buf)RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+
+        case kPixelBGRResv8BitPerColor: 
+            pixel_writer = new(pixel_writer_buf)BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+    } 
+        
     // fill white
     for(int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
         for(int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-            WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+            pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
 
     // fill a rectangle
     for(int x = 0; x < 200; x++) {
         for(int y = 0; y < 100; y++) {
-            WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+            pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
 
