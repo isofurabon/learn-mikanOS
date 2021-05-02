ce73ace6abf012bb3c3471c7d5974ae5fec3a435
isofurabon <isofurabon@users.noreply.github.com>
2021-05-02 08:21:13 +0000

    feat: update boot load sequences

--- chapter_4/MikanLoaderPkg/Main.c
+++ chapter_4/MikanLoaderPkg/Main.c
@@ -3,12 +3,14 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/PrintLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
 #include <Guid/FileInfo.h>
 #include "frame_buffer_config.hpp"
+#include "elf.h"
 
 // MemoryMap for EFI_MEMOTRY_DESCRIPTOR
 struct MemoryMap {
@@ -182,6 +184,35 @@ void Halt(void){
     while(1) __asm__("hlt");
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first, UINT64* last) {
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    *first = MAX_UINT64;
+    *last = 0;
+    for(Elf64_Half i = 0; i < ehdr->e_phnum; ++i){
+        if (phdr[i].p_type != PT_LOAD) {
+            continue;
+        }
+        *first = MIN(*first, phdr[i].p_vaddr);
+        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+    }
+}
+
+void CopyLoadSegments(Elf64_Ehdr* ehdr) {
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    for(Elf64_Half i = 0; i < ehdr->e_phnum; ++i){
+        // if segment type is not loadable.
+        if (phdr[i].p_type != PT_LOAD) {
+            continue;
+        }
+
+        UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+        CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+
+        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+        SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+    }
+}
+
 EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 {
     EFI_STATUS status;
@@ -267,20 +298,42 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
-    // allocate page for kernel
-    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    UINTN allocate_page_size = (kernel_file_size + 0xfff ) / 0x1000; // 4KiB align
-    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, allocate_page_size, &kernel_base_addr);
+    // allocate temporary pool for kernel
+    VOID* kernel_buffer;
+    status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
     if (EFI_ERROR(status)){
-        Print(L"failed to allocate pages: %r", status);
+        Print(L"failed to allocate pool: %r", status);
         Halt();
     }
 
-    // load kernel to allocated memory
-    status = kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
-    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+    // load kernel to allocated pooled buf
+    status = kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_buffer);
     if (EFI_ERROR(status)){
         Print(L"error: %r", status);
+        Halt();
+    }
+
+    // calc load address range
+    Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+    UINT64 kernel_first_addr, kernel_last_addr;
+    CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+
+    // allocate page for kernel
+    UINTN num_pages = ( kernel_last_addr - kernel_first_addr + 0xfff ) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
+    if (EFI_ERROR(status)){
+        Print(L"failed to allocate page: %r\n", status);
+        Halt();
+    }
+
+    // copy segment to allocated page
+    CopyLoadSegments(kernel_ehdr);
+    Print(L"Kernel:0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+    // free pool (buf)
+    status = gBS->FreePool(kernel_buffer);
+    if (EFI_ERROR(status)){
+        Print(L"failed to free pool: %r\n", status);
         Halt();
     }
 
@@ -302,7 +355,7 @@ EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
     /* Boot Kernel */
     // set a pointer address to kernel_base_addr + 24 in entry_addr.
 
-    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+    UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
     // get frame buffer config
     struct FrameBufferConfig config = {
