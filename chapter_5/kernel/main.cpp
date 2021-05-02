9f3ffafd4f0b667debfc8407e452a8bafdaf6173
isofurabon <isofurabon@users.noreply.github.com>
2021-05-02 08:36:31 +0000

    feat: writer A

--- chapter_5/kernel/main.cpp
+++ chapter_5/kernel/main.cpp
@@ -2,6 +2,25 @@
 #include <cstddef>
 #include "frame_buffer_config.hpp"
 
+const uint8_t kFontA[16] = {
+    0b00000000,
+    0b00011000,
+    0b00011000,
+    0b00011000,
+    0b00011000,
+    0b00100100,
+    0b00100100,
+    0b00100100,
+    0b00100100,
+    0b01111110,
+    0b01000010,
+    0b01000010,
+    0b01000010,
+    0b11100111,
+    0b00000000,
+    0b00000000,
+};
+
 struct PixelColor {
     uint8_t r, g, b;
 };
@@ -10,10 +29,10 @@ class PixelWriter {
 public:
     PixelWriter(const FrameBufferConfig& config) : m_config(config){}
     virtual ~PixelWriter() = default;
-    virtual void Write(int x, int y, const PixelColor& c) = 0;
+    virtual void Write(int x, int y, const PixelColor& c) const = 0;
 
 protected:
-    uint8_t* PixelAt(int x, int y){
+    uint8_t* PixelAt(int x, int y) const {
         return m_config.frame_buffer + 4 * (m_config.pixels_per_scan_line * y + x);
     }
 
@@ -26,7 +45,7 @@ public:
     // https://cpprefjp.github.io/lang/cpp11/inheriting_constructors.html
     using PixelWriter::PixelWriter;
 
-    virtual void Write(int x, int y, const PixelColor& c) override {
+    virtual void Write(int x, int y, const PixelColor& c) const override {
         auto* p = PixelAt(x, y);
         p[0] = c.r;
         p[1] = c.g;
@@ -38,13 +57,27 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
 public:
     using PixelWriter::PixelWriter;
 
-    virtual void Write(int x, int y, const PixelColor& c) override {
+    virtual void Write(int x, int y, const PixelColor& c) const override {
         auto* p = PixelAt(x, y);
         p[0] = c.b;
         p[1] = c.g;
         p[2] = c.r;
     }
 };
+
+void WriteAscii(const PixelWriter& writer, int x, int y, char c, const PixelColor& color){
+    if ( c != 'A' ){
+        return;
+    }
+
+    for(int dy = 0; dy < 16; ++dy){
+        for(int dx = 0; dx < 8; ++dx){
+            if ((kFontA[dy] << dx) & 0x80u){
+                writer.Write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
 
 void* operator new(size_t size, void* buf) {
     return buf;
@@ -83,6 +116,9 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
             pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
+
+    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
 
     while (1) __asm__("hlt");
 }
