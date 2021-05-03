ebfe979e7dda445700718339bdd75e48faa2448d
isofurabon <isofurabon@users.noreply.github.com>
2021-05-03 07:08:32 +0000

    feat: write all ascii characters

--- chapter_5/kernel/main.cpp
+++ chapter_5/kernel/main.cpp
@@ -41,8 +41,11 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
         }
     }
 
-    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+    // write all ascii characters
+    int i = 0;
+    for(char c = '!'; c<= '~'; ++c, ++i){
+        WriteAscii(*pixel_writer, 8*i, 50, c, {0, 0, 0});
+    }
 
     while (1) __asm__("hlt");
 }
