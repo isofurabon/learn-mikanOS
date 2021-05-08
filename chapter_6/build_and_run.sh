a9551da6ce625ed2ae339c23294afef616136334
isofurabon <isofurabon@users.noreply.github.com>
2021-05-08 07:52:26 +0000

    feat: add chapter_6 - add draw/fill rectangle

---
+++ chapter_6/build_and_run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_ROOT="/learn-mikanos"
+CHAPTER="chapter_6"
+VM_ROOT="${PROJECT_ROOT}/mnt"
+EDK2_DIR="${HOME}/edk2"
+TARGET_CONFIG_FILE="${PROJECT_ROOT}/config/target.txt"
+OSBOOK_DIR=${HOME}/osbook
+
+# build kernel
+(source ${OSBOOK_DIR}/devenv/buildenv.sh && make -C ${PROJECT_ROOT}/${CHAPTER}/kernel all)
+
+# create symbolic link in edk2 directory
+ln -fs ${PROJECT_ROOT}/${CHAPTER}/MikanLoaderPkg ${EDK2_DIR}
+
+# copy target.txt to edk2 dir
+cp -f ${TARGET_CONFIG_FILE} ${EDK2_DIR}/Conf/target.txt
+
+# source edk & build
+(cd ${EDK2_DIR} && source edksetup.sh && build)
+
+# move efi to vm_root
+cp -f ${EDK2_DIR}/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi ${VM_ROOT}/efi/boot/bootx64.efi
+
+# move kernel to vm_root
+cp -f ${PROJECT_ROOT}/${CHAPTER}/kernel/kernel.elf ${VM_ROOT}/kernel.elf
+
+# run
+${PROJECT_ROOT}/tools/run_qemu.sh ${VM_ROOT}
---
+++ chapter_6/kernel/main.cpp
@@ -0,0 +1,64 @@
+#include <cstdint>
+#include <cstddef>
+#include <cstdio>
+#include "graphics.hpp"
+#include "font.hpp"
+#include "console.hpp"
+
+void* operator new(size_t size, void* buf) {
+    return buf;
+}
+
+void operator delete(void* obj) noexcept {
+}
+
+
+char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
+PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
+
+int printk(const char* format, ...){
+    va_list ap;
+    int result;
+    char s[1024];
+
+    va_start(ap, format);
+    result = vsprintf(s, format, ap);
+    va_end(ap);
+
+    console->PutString(s);
+    return result;
+}
+
+
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) 
+{
+    // create pixel writer
+    switch(frame_buffer_config.pixel_format){
+        case kPixelRGBResv8BitPerColor: 
+            pixel_writer = new(pixel_writer_buf)RGBResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+
+        case kPixelBGRResv8BitPerColor: 
+            pixel_writer = new(pixel_writer_buf)BGRResv8BitPerColorPixelWriter{frame_buffer_config};
+            break;
+    } 
+
+    // create console
+    console = new(console_buf)Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+        
+    // fill white
+    for(int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
+        for(int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
+            pixel_writer->Write(x, y, {255, 255, 255});
+        }
+    }
+
+    for (int i = 0; i < 27; ++i){
+        printk("printk: %d\n", i);
+    }
+
+    while (1) __asm__("hlt");
+}
