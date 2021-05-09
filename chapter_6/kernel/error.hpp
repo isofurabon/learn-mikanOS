4887fb2afa48a2712ba18af5e2ede2d4a3a0d489
isofurabon <isofurabon@users.noreply.github.com>
2021-05-09 06:01:58 +0000

    feat: add error

---
+++ chapter_6/kernel/error.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <array>
+
+class Error {
+public:
+    enum Code {
+        Success,
+        Full,
+        Empty,
+        LastOfCode,
+    };
+
+    Error(const Code code): m_code{code} {}
+
+    operator bool() const {
+        return this->m_code != Success;
+    }
+
+    const char* Name() const {
+        return code_names[static_cast<size_t>(this->m_code)];
+    }
+
+private:
+    static constexpr std::array<const char*, static_cast<size_t>(LastOfCode)> code_names = {
+        "Success",
+        "Full",
+        "Empty",
+    };
+
+    Code m_code;
+};
