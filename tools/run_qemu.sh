d229aed054412e6048aee927db4eb59c6f4f80c1
isofurabon <isofurabon@users.noreply.github.com>
2021-05-16 07:23:40 +0000

    fix: fix run scripts

--- tools/run_qemu.sh
+++ tools/run_qemu.sh
@@ -12,3 +12,6 @@ qemu-system-x86_64 \
     -drive if=pflash,format=raw,readonly,file=${HOME}/osbook/devenv/OVMF_CODE.fd \
     -drive if=pflash,format=raw,file=${HOME}/osbook/devenv/OVMF_VARS.fd \
     -drive format=raw,file=fat:rw:${FS_PATH} \
+    -device nec-usb-xhci,id=xhci \
+    -device usb-mouse -device usb-kbd \
+    -monitor stdio \
