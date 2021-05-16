24b3cb30aeac450fba12a2f0774b7e5e15229873
isofurabon <isofurabon@users.noreply.github.com>
2021-05-16 06:45:40 +0000

    feat: fix typo

--- chapter_6/kernel/libcxx_support.cpp
+++ chapter_6/kernel/libcxx_support.cpp
@@ -5,6 +5,6 @@ std::new_handler std::get_new_handler() noexcept {
     return nullptr;
 }
 
-extern "C" int posix_memaglign(void**, size_t, size_t) {
+extern "C" int posix_memalign(void**, size_t, size_t) {
     return ENOMEM;
 }
