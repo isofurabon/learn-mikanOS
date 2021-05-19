3cca437369ed2ff36f518c03fe4bea15a00815f1
isofurabon <isofurabon@users.noreply.github.com>
2021-05-19 15:14:38 +0000

    feat: add build and run scripts - delete build_and_run.sh

---
+++ chapter_2/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+WORKSPACE_ROOT="${GITHUB_WORKSPACE:-/learn-mikanos}"
+VM_ROOT="${WORKSPACE_ROOT}/vm"
+EDK2_DIR="${HOME}/edk2"
+
+# create symbolic link in edk2 directory
+
+ln -fs $(realpath ./MikanLoaderPkg) ${EDK2_DIR}
+
+# copy target.txt to edk2 dir
+cp -f ./MikanLoaderPkg/target.txt ${EDK2_DIR}/Conf/target.txt
+
+# source edk & build
+rm -fr ${EDK2_DIR}/Build/*
+(cd ${EDK2_DIR} && source edksetup.sh && build)
+
+echo "Build Complete!!"
