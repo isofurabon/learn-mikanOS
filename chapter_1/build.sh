d8721cc9dcb24334d358eb417c61b651f65dc862
isofurabon <isofurabon@users.noreply.github.com>
2021-05-19 15:33:48 +0000

    fix: rm with -f

--- chapter_1/build.sh
+++ chapter_1/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-rm *.o *.efi
+rm -f *.o *.efi
 clang -target x86_64-pc-win32-coff -mno-red-zone -fno-stack-protector -fshort-wchar -Wall -c hello.c
 lld-link /subsystem:efi_application /entry:EfiMain /out:hello.efi hello.o
