9f251167385eb68e1d4d2fb6c7e04d9705f11c0f
isofurabon <isofurabon@users.noreply.github.com>
2021-05-20 13:42:26 +0000

    fix: delete RUN

--- .github/actions/build-all-chapters/entrypoint.sh
+++ .github/actions/build-all-chapters/entrypoint.sh
@@ -3,16 +3,16 @@ set -e
 
 # clone repos & setup build envs
 # build EDK II
-RUN git clone --recursive https://github.com/tianocore/edk2.git ${HOME}/edk2 \
+git clone --recursive https://github.com/tianocore/edk2.git ${HOME}/edk2 \
  && (cd ${HOME}/edk2 && git checkout 38c8be123aced4cc8ad5c7e0da9121a181b94251) \
  && make -C ${HOME}edk2/BaseTools/Source/C
 #  && rm -rf edk2
 
 # clone mikanos devenv
-RUN git clone https://github.com/uchan-nos/mikanos-build.git ${HOME}/osbook
+git clone https://github.com/uchan-nos/mikanos-build.git ${HOME}/osbook
 
 # download standard libraries
-RUN curl -L https://github.com/uchan-nos/mikanos-build/releases/download/v2.0/x86_64-elf.tar.gz \
+curl -L https://github.com/uchan-nos/mikanos-build/releases/download/v2.0/x86_64-elf.tar.gz \
   | tar xzvf - -C ${HOME}/osbook/devenv
 
 # Build all
