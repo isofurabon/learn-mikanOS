76511e5fe6df674dbec1aec37e85f668a78383d2
isofurabon <isofurabon@users.noreply.github.com>
2021-05-23 05:45:44 +0000

    feat: fix typo

--- chapter_7/kernel/interrupt.cpp
+++ chapter_7/kernel/interrupt.cpp
@@ -16,5 +16,5 @@ void SetIDTEntry(InterruptDescriptor& desc,
 
 void NotifyEndOfInterrupt() {
     volatile auto end_of_interrupt = reinterpret_cast<uint32_t*>(0xfee000b0);
-    end_of_interrupt = 0;
+    *end_of_interrupt = 0;
 }
