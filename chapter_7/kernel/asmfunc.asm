6929ef8790eb377d8089259493b63000b9dc2b3a
isofurabon <isofurabon@users.noreply.github.com>
2021-05-23 04:41:48 +0000

    implement GetCS and LoadIDT

--- chapter_7/kernel/asmfunc.asm
+++ chapter_7/kernel/asmfunc.asm
@@ -18,3 +18,21 @@ IoIn32:
     mov dx, di    ; dx = addr
     in eax, dx
     ret
+
+global GetCS ; uint16_t GetCS(void);
+GetCS:
+    xor eax, eax
+    mov ax, cs;
+    ret
+
+global LoadIDT  ; void LoadIDT(uint16_t limit, uint64_t offset);
+LoadIDT:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10
+    mov [rsp], di
+    mov [rsp + 2], rsi
+    lidt [rsp]
+    mov rsp, rbp
+    pop rbp
+    ret
