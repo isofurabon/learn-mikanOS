4be3b819c221d89531e24bf9abb23ce82c3cd34f
isofurabon <isofurabon@users.noreply.github.com>
2021-05-23 05:35:04 +0000

    feat: enable interrupt for mouse

--- chapter_7/kernel/main.cpp
+++ chapter_7/kernel/main.cpp
@@ -16,6 +16,8 @@
 #include "usb/classdriver/mouse.hpp"
 #include "usb/xhci/xhci.hpp"
 #include "usb/xhci/trb.hpp"
+#include "interrupt.hpp"
+#include "asmfunc.h"
 
 void operator delete(void* obj) noexcept {
 }
@@ -70,6 +72,18 @@ void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
     pci::WriteConfReg(xhc_dev, 0xd0, ehci2xhci_ports);
 
     Log(kDebug, "SwitchEhci2Xhci: S = %02x, xHCI = %02x \n", superspeed_ports, ehci2xhci_ports);
+}
+
+usb::xhci::Controller* xhc;
+
+__attribute__((interrupt))
+void IntHandlerXHCI(InterruptFrame* frame) {
+    while (xhc->PrimaryEventRing()->HasFront()) {
+        if (auto err = ProcessEvent(*xhc)) {
+            Log(kError, "Error while ProcessEvent: %s at %s:%d\n", err.Name(), err.File(), err.Line());
+        }
+    }
+    NotifyEndOfInterrupt();
 }
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) 
@@ -136,6 +150,20 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
         Log(kInfo, "xHC has been found: %d.%d.%d\n", xhc_dev->bus, xhc_dev->device, xhc_dev->function);
     }
 
+    //setup idt
+    const uint16_t cs = GetCS();
+    SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerXHCI), cs);
+
+    // Load (Register) idt to cpu
+    LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
+    
+    // setup MSI interrupt 
+    // get bsp (bootstrap processor) local apic
+    const uint8_t bsp_local_apic_id = *reinterpret_cast<const uint32_t*>(0xfee00020) >> 24;
+    pci::ConfigureMSIFixedDestination(*xhc_dev, bsp_local_apic_id, 
+        pci::MSITriggerMode::kLevel, pci::MSIDeliveryMode::kFixed, 
+        InterruptVector::kXHCI, 0);
+
     // read bar register
     const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
     Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
@@ -156,6 +184,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
     Log(kInfo, "xHC starting\n");
     xhc.Run();
 
+    ::xhc = &xhc;
+    __asm__("sti");
+
+
     usb::HIDMouseDriver::default_observer = MouseObserver;
 
     for(int i = 1; i <= xhc.MaxPorts(); ++i){
@@ -167,12 +199,6 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
                 Log(kError, "failed to confgure port: %s at %s:%d\n", err.Name(), err.File(), err.Line());
                 continue;
             }
-        }
-    }
-
-    while (1) {
-        if (auto err = ProcessEvent(xhc)) {
-            Log(kError, "Error while ProcessEvent: %s at %s : %d\n", err.Name(), err.File(), err.Line());
         }
     }
 
