6f0d29b474a087451e13bfdd30997b4c3c4df972
isofurabon <isofurabon@users.noreply.github.com>
2021-06-01 15:20:30 +0000

    feat: print memory_map information

--- chapter_8/kernel/main.cpp
+++ chapter_8/kernel/main.cpp
@@ -5,6 +5,8 @@
 #include <numeric>
 #include <vector>
 
+#include "frame_buffer_config.hpp"
+#include "memory_map.hpp"
 #include "graphics.hpp"
 #include "mouse.hpp"
 #include "font.hpp"
@@ -91,7 +93,7 @@ void IntHandlerXHCI(InterruptFrame* frame) {
     NotifyEndOfInterrupt();
 }
 
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) 
+extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const MemoryMap& memory_map) 
 {
     // create pixel writer
     switch(frame_buffer_config.pixel_format){
@@ -118,6 +120,30 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config)
     printk("Welcome to MikanOS!\n");
 
     SetLogLevel(kWarn);
+
+    // Print memory_map info
+    const std::array available_memory_types{
+        MemoryType::kEfiBootServicesCode,
+        MemoryType::kEfiBootServicesData,
+        MemoryType::kEfiConventionalMemory,
+    };
+
+    printk("memory_map: %p\n", &memory_map);
+    for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
+        iter < reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
+        iter += memory_map.descriptor_size) {
+        auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
+        for (int i = 0; i < available_memory_types.size(); ++i) {
+        if (desc->type == available_memory_types[i]) {
+            printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+                desc->type,
+                desc->physical_start,
+                desc->physical_start + desc->number_of_pages * 4096 - 1,
+                desc->number_of_pages,
+                desc->attribute);
+        }
+        }
+    }
     
     // Setup mouse
     mouse_cursor = new(mouse_cursor_buf) MouseCursor{
