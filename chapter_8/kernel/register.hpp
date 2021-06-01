ce4cb99bebcd853b3f5c3ce2afe5b277c2045cbe
isofurabon <isofurabon@users.noreply.github.com>
2021-06-02 00:58:47 +0900

    feat: implement osbook_day08a (#7)
    * chore: copy chapter_7 to chapter_8
    
    * chore: copy a looader from chapter_4
    
    * feat: update a path for loader in build.sh
    
    * feat: toss memmap in entrytype
    
    * feat: add memory_map.hpp
    
    * fix: fix typo
    
    * feat: print memory_map information

---
+++ chapter_8/kernel/register.hpp
@@ -0,0 +1,69 @@
+/* Read/Write MMIO */
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+template <typename T>
+struct ArrayLength {};
+
+template <typename T, size_t N>
+struct ArrayLength<T[N]> {
+    static constexpr size_t value = N;
+};
+
+template <typename T>
+class MemMapRegister {
+public:
+    T Read() const {
+        T tmp;
+        for (size_t i = 0; i < m_len; ++i) {
+            tmp.data[i] = m_value.data[i];
+        }
+        return tmp;
+    }
+
+    void Write(const T& value) {
+        T tmp;
+        for (size_t i = 0; i < m_len; ++i) {
+            m_value.data[i] = value.data[i];
+        }
+    }
+
+private:
+    volatile T m_value;
+    static const size_t m_len = ArrayLength<decltype(T::data)>::value;
+};
+
+template <typename T>
+struct DefaultBitmap {
+    T data[1];
+
+    DefaultBitmap& operator =(const T& value) {
+        data[0] = value;
+    }
+    operator T() const { return data[0]; }
+};
+
+template <typename T>
+class ArrayWrapper {
+public:
+    using ValueType = T;
+    using Iterator = ValueType*;
+    using ConstIterator = const ValueType*;
+
+    ArrayWrapper(uintptr_t array_base_addr, size_t size) : m_array(reinterpret_cast<ValueType*>(array_base_addr)), m_size(size) {}
+
+    size_t Size() const { return m_size; }
+
+    Iterator begin() { return m_array; }
+    Iterator end() { return m_array + m_size; }
+    ConstIterator cbegin() const { return m_array; }
+    ConstIterator cend() const { return m_array + m_size; }
+
+    ValueType& operator [](size_t index) { return m_array[index]; }
+
+private:
+    ValueType* const m_array;
+    const size_t m_size;
+};
