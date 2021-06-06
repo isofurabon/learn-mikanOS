c9093f6153fc2614eef4cc1eb5ddfce6f018c3d2
isofurabon <isofurabon@users.noreply.github.com>
2021-06-06 23:43:16 +0900

    feat: osbook_day08b (#9)
    * feat: move stack
    
    * feat: add IsAvailable to allocate free memory
    
    * feat: add segment.hpp/cpp
    
    * feat: setup gdt
    
    * feat: enable paging in os
    
    * fix: revert Main.c in chapter_3

--- chapter_8/kernel/main.cpp
+++ chapter_8/kernel/main.cpp
@@ -21,6 +21,8 @@
 #include "interrupt.hpp"
 #include "asmfunc.h"
 #include "queue.hpp"
+#include "segment.hpp"
+#include "paging.hpp"
 
 void operator delete(void* obj) noexcept {
 }
@@ -93,8 +95,13 @@ void IntHandlerXHCI(InterruptFrame* frame) {
     NotifyEndOfInterrupt();
 }
 
-extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const MemoryMap& memory_map) 
+alignas(16) uint8_t kernel_main_stack[1024 * 1024];
+
+extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_ref, const MemoryMap& memory_map_ref) 
 {
+    FrameBufferConfig frame_buffer_config{frame_buffer_config_ref};
+    MemoryMap memory_map{memory_map_ref};
+
     // create pixel writer
     switch(frame_buffer_config.pixel_format){
         case kPixelRGBResv8BitPerColor: 
@@ -121,27 +128,32 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const M
 
     SetLogLevel(kWarn);
 
-    // Print memory_map info
-    const std::array available_memory_types{
-        MemoryType::kEfiBootServicesCode,
-        MemoryType::kEfiBootServicesData,
-        MemoryType::kEfiConventionalMemory,
-    };
+    /* Setup Segment From Here */
+    SetupSegments();
 
-    printk("memory_map: %p\n", &memory_map);
-    for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
-        iter < reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
-        iter += memory_map.descriptor_size) {
+    const uint16_t kernel_cs = 1 << 3;
+    const uint16_t kernel_ss = 2 << 3;
+    SetDSAll(0);
+    SetCSSS(kernel_cs, kernel_ss);
+
+    /* Setup Segment To Here */
+
+    /* Setup Paging From Here */
+    SetupIdentityPageTable();
+    /* Setup Paging To Here */
+
+    // Print memory_map info
+    const auto memory_map_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
+    for (uintptr_t iter = memory_map_base; iter < memory_map_base + memory_map.map_size; iter += memory_map.descriptor_size){
         auto desc = reinterpret_cast<MemoryDescriptor*>(iter);
-        for (int i = 0; i < available_memory_types.size(); ++i) {
-        if (desc->type == available_memory_types[i]) {
-            printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
-                desc->type,
-                desc->physical_start,
-                desc->physical_start + desc->number_of_pages * 4096 - 1,
-                desc->number_of_pages,
-                desc->attribute);
-        }
+
+        if (IsAvailable(static_cast<MemoryType>(desc->type))) {
+        printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+            desc->type,
+            desc->physical_start,
+            desc->physical_start + desc->number_of_pages * 4096 - 1,
+            desc->number_of_pages,
+            desc->attribute);
         }
     }
     
@@ -186,8 +198,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config, const M
     }
 
     //setup idt
-    const uint16_t cs = GetCS();
-    SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerXHCI), cs);
+    SetIDTEntry(idt[InterruptVector::kXHCI], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerXHCI), kernel_cs);
 
     // Load (Register) idt to cpu
     LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
