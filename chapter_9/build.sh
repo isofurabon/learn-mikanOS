979593bd994df59542f3ffea2719f376ffde1f65
isofurabon <isofurabon@users.noreply.github.com>
2021-06-16 15:28:56 +0000

    chore: remove bootloader and update build script

--- chapter_9/build.sh
+++ chapter_9/build.sh
@@ -16,10 +16,10 @@ ln -fs ${WORKSPACE_ROOT}/common/usb ./kernel/usb
 (source ${OSBOOK_DIR}/devenv/buildenv.sh && make -C ./kernel clean && make -C ./kernel all)
 
 # create symbolic link in edk2 directory
-ln -fs $(realpath ./MikanLoaderPkg) ${EDK2_DIR}
+ln -fs $(realpath ../chapter_8/MikanLoaderPkg) ${EDK2_DIR}
 
 # copy target.txt to edk2 dir
-cp -f ../chapter_4/MikanLoaderPkg/target.txt ${EDK2_DIR}/Conf/target.txt
+cp -f ../chapter_8/MikanLoaderPkg/target.txt ${EDK2_DIR}/Conf/target.txt
 
 # source edk & build
 rm -fr ${EDK2_DIR}/Build/*
