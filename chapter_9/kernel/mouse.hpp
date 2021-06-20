f0c8bba01cb853c9c532cee3c46a8f0c075fd965
isofurabon <isofurabon@users.noreply.github.com>
2021-06-20 07:02:59 +0000

    feat: update mouse

--- chapter_9/kernel/mouse.hpp
+++ chapter_9/kernel/mouse.hpp
@@ -1,13 +1,8 @@
 #pragma once
 #include "graphics.hpp"
 
-class MouseCursor {
-public:
-    MouseCursor(PixelWriter* writer, const PixelColor erase_color, const Vector2D<int> initial_position);
-    void MoveRelative(const Vector2D<int> displacement);
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const PixelColor kMouseTransparentColor{0, 0, 1};
 
-private:
-    PixelWriter* m_pixel_writer = nullptr;
-    PixelColor m_erase_color;
-    Vector2D<int> m_position;
-};
+void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position);
